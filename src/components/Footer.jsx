fc1ddc18ea7cf3b869f85e2e5e0e8b7b217a229d
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-05-02 02:23:57 +0530

    design completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 const Footer = () => {
+  const [email, setEmail] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const handleEmailChange = (e) => {
+    // e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setIsValid(e.target.checkValidity());
+    setEmail("");
+  };
+
   return (
     <footer className="footer">
       <div className="bg-footer"></div>
@@ -7,12 +24,17 @@ const Footer = () => {
           <h2>Get notified when we launch</h2>
         </div>
 
-        <form>
+        <form onSubmit={handleSubmit} noValidate>
           <input
             placeholder="Email address"
             type="email"
             name="email"
             id="email"
+            onChange={handleEmailChange}
+            value={email}
+            style={{
+              border: isValid ? "2px solid #3ee9e5" : "2px solid #ff2965",
+            }}
           />
           <button type="submit">Get notified</button>
         </form>
